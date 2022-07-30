b795a9dfc13bbd1c51578493ce7cd99ddcdd11cf
Crowdbotics <team@crowdbotics.com>
2022-07-30 23:58:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/inspect_hoa_36521/MainActivity.java
@@ -0,0 +1,15 @@
+package com.inspect_hoa_36521;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "inspect_hoa_36521";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "inspect_hoa_36521",
+  url: "https://inspect_hoa_36521.botics.co",
+  api: "https://inspect_hoa_36521.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
